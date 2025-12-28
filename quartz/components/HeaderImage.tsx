b2ed1ec79b6cafe4ab026e90b7cfd44fbf0197a7
SolveSaint <solvesaint5@gmail.com>
2025-12-28 15:39:41 -0500

    Deploy AVSV: 2025-12-28 15:39:41

--- quartz/components/HeaderImage.tsx
+++ quartz/components/HeaderImage.tsx
@@ -4,7 +4,7 @@ const HeaderImage: QuartzComponent = () => {
   return (
     <div style={{ width: "100%", textAlign: "center", marginBottom: "1rem" }}>
       <img
-        src="https://solvesaint.github.io/AliensVSVeterans/static/NHI_Connection_Banner_Cropped.png"
+        src="https://solvesaint.github.io/AliensVSVeterans/static/header.png"
         alt="NHI Connection banner"
         style={{ maxWidth: "100%", height: "auto" }}
 />
