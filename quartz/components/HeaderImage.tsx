f6b8e86250d8df2d021e0e2413f060dc658bf0ec
SolveSaint <solvesaint5@gmail.com>
2025-12-28 14:51:17 -0500

    Deploy AVSV: 2025-12-28 14:51:17

--- quartz/components/HeaderImage.tsx
+++ quartz/components/HeaderImage.tsx
@@ -4,7 +4,7 @@ const HeaderImage: QuartzComponent = () => {
   return (
     <div style={{ width: "100%", textAlign: "center", marginBottom: "1rem" }}>
       <img
-        src="/static/NHI_Connection Banner Cropped.png"
+        src="/quartz/static/NHI_Connection Banner Cropped.png"
         alt="NHI Connection banner"
         style={{ maxWidth: "100%", height: "auto" }}
       />
