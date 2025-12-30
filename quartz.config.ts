5937afb5e3af1e0089bf2f7ebe7dbfef930436e2
SolveSaint <solvesaint5@gmail.com>
2025-12-29 19:57:05 -0500

    Deploy AVSV: 2025-12-29 19:57:05

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "AliensVSVeterans",
+    pageTitle: "AVSV",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
